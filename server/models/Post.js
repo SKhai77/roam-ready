67b69472206bd4ce488a993cb80eb2692b8cd020
SKhai77 <orionkhai@gmail.com>
2023-11-10 17:13:01 -0600

    Update the username, title, description, and image to aligned the names with other files

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,19 +1,26 @@
-const { model, Schema } = require('mongoose');
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const postSchema = new Schema({
-  title: {
+  postTitle: {
     type: String,
-    required: true,
+    required: "You need to give a title!",
+    minlength: 1,
+    maxlength: 280,
     trim: true,
   },
-  description: {
+  postImage: {
     type: String,
-    required: true,
     trim: true,
   },
-  image: {
+  postText: {
+    type: String,
+    required: "You need to leave a post!",
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  postAuthor: {
     type: String,
     required: true,
     trim: true,
@@ -44,19 +51,24 @@ const postSchema = new Schema({
   ],
   likes: [
     {
-      username: String,
-      required: true,
-      trim: true,
-      createdAt: String
-    }
+      postAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
   ],
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
-  }
+    ref: "User",
+  },
 });
 
-const Post = model('Thought', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;
-
