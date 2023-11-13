6ac245395e171b7704b76a287d7914872da3fa60
SKhai77 <orionkhai@gmail.com>
2023-11-13 00:09:38 -0600

    Added likes in the schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -66,26 +66,19 @@ const resolvers = {
       throw AuthenticationError;
       ("You need to be logged in!");
     },
-    likePost: async (parent, { postId }, context) => {
+    removePost: async (parent, { postId }, context) => {
       if (context.user) {
-        const post = await Post.findById(postId);
-        if (post) {
-          if (post.likes.find((like) => like.username === username)) {
-            // Post already likes, unlike it
-            post.likes = post.likes.filter(
-              (like) => like.username !== username
-            );
-          } else {
-            // Not liked, like post
-            post.likes.push({
-              username,
-              createdAt: new Date().toISOString(),
-            });
-          }
+        const post = await Post.findOneAndDelete({
+          _id: postId,
+          postAuthor: context.user.username,
+        });
 
-          await post.save();
-          return post;
-        } else throw new UserInputError("Post not found");
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { posts: post._id } }
+        );
+
+        return post;
       }
       throw AuthenticationError;
     },
@@ -106,22 +99,6 @@ const resolvers = {
       }
       throw AuthenticationError;
     },
-    removePost: async (parent, { postId }, context) => {
-      if (context.user) {
-        const post = await Post.findOneAndDelete({
-          _id: postId,
-          postAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { posts: post._id } }
-        );
-
-        return post;
-      }
-      throw AuthenticationError;
-    },
     removeComment: async (parent, { postId, commentId }, context) => {
       if (context.user) {
         return Post.findOneAndUpdate(
@@ -131,6 +108,41 @@ const resolvers = {
               comments: {
                 _id: commentId,
                 commentAuthor: context.user.username,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+
+    addLike: async (parent, { postId, likeText }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: {
+              likes: { likeText, likeAuthor: context.user.username },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError;
+    },
+    removeLike: async (parent, { postId, likeId }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $pull: {
+              likes: {
+                _id: likeId,
+                likeAuthor: context.user.username,
               },
             },
           },
